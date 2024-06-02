b627b1bcb5fcec29da9eae5a8b424935a0a961c2
Polina <ms.maylin@inbox.ru>
2024-06-02 19:02:59 +0300

    checkstyle plugin added

--- src/main/java/org/example/CompareTwoLists.java
+++ src/main/java/org/example/CompareTwoLists.java
@@ -1,27 +1,23 @@
 package org.example;
-
 public class CompareTwoLists {
+  NumList list1;
+  NumList list2;
 
-    NumList list1;
-    NumList list2;
+  public CompareTwoLists(NumList list1, NumList list2) {
+    this.list1 = list1;
+    this.list2 = list2;
+  }
 
-    public CompareTwoLists(NumList list1, NumList list2) {
-        this.list1 = list1;
-        this.list2 = list2;
+  public void byArithmeticMean() {
+    Double arithmeticMean1 = list1.calculateTheArithmeticMeanOfTheList();
+    Double arithmeticMean2 = list2.calculateTheArithmeticMeanOfTheList();
+
+    if (arithmeticMean1 > arithmeticMean2) {
+      System.out.println("Первый список имеет большее среднее значение");
+    } else if (arithmeticMean1.equals(arithmeticMean2)) {
+      System.out.println("Средние значения равны");
+    } else {
+      System.out.println("Второй список имеет большее среднее значение");
     }
-
-    public void byArithmeticMean() {
-        Double arithmeticMean1 = list1.calculateTheArithmeticMeanOfTheList();
-        Double arithmeticMean2 = list2.calculateTheArithmeticMeanOfTheList();
-
-        if (arithmeticMean1 > arithmeticMean2){
-            System.out.println("Первый список имеет большее среднее значение");
-        }
-        else if (arithmeticMean1.equals(arithmeticMean2)){
-            System.out.println("Средние значения равны");
-        }
-        else {
-            System.out.println("Второй список имеет большее среднее значение");
-        }
-    }
+  }
 }
--- src/main/java/org/example/NumList.java
+++ src/main/java/org/example/NumList.java
@@ -1,32 +1,32 @@
 package org.example;
 
 import java.util.List;
+
 public class NumList {
+  private List<Double> list;
 
-    private List<Double> list;
+  public NumList(List<Double> list) {
+    this.list = list;
+  }
 
-    public NumList(List<Double> list) {
-        this.list = list;
+  public Double calculateTheArithmeticMeanOfTheList() {
+    double sum = 0;
+    for (Double aDouble : list) {
+      sum += aDouble;
     }
+    return sum / list.size();
+  }
 
-    public Double calculateTheArithmeticMeanOfTheList() {
-        double sum = 0;
-        for (Double aDouble : list) {
-            sum += aDouble;
-        }
-        return sum / list.size();
-    }
+  @Override
+  public String toString() {
+    return "list = " + list;
+  }
 
-    @Override
-    public String toString() {
-        return  "list = " + list;
-    }
+  public List<Double> getList() {
+    return list;
+  }
 
-    public List<Double> getList() {
-        return list;
-    }
-
-    public void setList(List<Double> list) {
-        this.list = list;
-    }
+  public void setList(List<Double> list) {
+    this.list = list;
+  }
 }
